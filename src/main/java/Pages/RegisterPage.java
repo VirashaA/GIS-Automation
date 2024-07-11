4871606db332aebe40b84ab1f2ee51d971c519a3
Virasha <v.amaraweera@natpower.com>
2024-07-11 10:26:31 +0100

    second commit

--- src/main/java/Pages/RegisterPage.java
+++ src/main/java/Pages/RegisterPage.java
@@ -1,4 +1,10 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class RegisterPage {
+
+    WebDriver driver;
+    WebDriverWait wait;
 }
--- src/test/java/Tests/RegisterTest.java
+++ src/test/java/Tests/RegisterTest.java
@@ -1,4 +1,16 @@
 package Tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import utils.WebDriverManager;
+
 public class RegisterTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverManager.getDriver();
+        driver.manage().window().maximize();
+        driver.get("https://natpower-gis-dev-application.azurewebsites.net/auth");
+    }
 }
